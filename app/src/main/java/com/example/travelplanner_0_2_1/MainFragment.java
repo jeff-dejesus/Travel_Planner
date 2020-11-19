16ba5150ebd86f316a83c68f1ade671ea70088e0
GregisKhan <35552175+GregisKhan@users.noreply.github.com>
2020-11-19 14:28:53 -0800

    testing commit

--- app/src/main/java/com/example/travelplanner_0_2_1/MainFragment.java
+++ app/src/main/java/com/example/travelplanner_0_2_1/MainFragment.java
@@ -68,4 +68,27 @@ public class MainFragment extends Fragment implements View.OnClickListener {
                 break;
         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
 }
