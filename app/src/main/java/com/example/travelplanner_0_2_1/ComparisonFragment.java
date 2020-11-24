663ce605e16dba5098641f6f7804bb2b0b80f7c3
GregisKhan <35552175+GregisKhan@users.noreply.github.com>
2020-11-24 10:45:19 -0800

    added graph to fragment_comparison.xml and dependency for graph. Also created basic UI for fragment_travel_planner.xml

--- app/src/main/java/com/example/travelplanner_0_2_1/ComparisonFragment.java
+++ app/src/main/java/com/example/travelplanner_0_2_1/ComparisonFragment.java
@@ -1,17 +1,33 @@
 package com.example.travelplanner_0_2_1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.travelplanner_0_2_1.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+
+import org.jetbrains.annotations.NotNull;
 
 //Todo: create UI for comparison
-public class ComparisonFragment extends Fragment {
+// the bar graph relies on a dependency the wiki is available here:
+//https://github.com/jjoe64/GraphView/wiki
+public class ComparisonFragment extends Fragment implements View.OnClickListener {
+
+    private NavController navController;
+    private GraphView graph;
+    private Button comparisonToPlanner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,5 +38,45 @@ public class ComparisonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_comparison, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NotNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        graph = (GraphView) view.findViewById(R.id.comparisonGraph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                //new DataPoint(0, -1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            }
+        });
+        series.setSpacing(50);
+
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
+
+        comparisonToPlanner = view.findViewById(R.id.comparisonToPlanner);
+        comparisonToPlanner.setOnClickListener(this);
+
+        navController = Navigation.findNavController(view);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.comparisonToPlanner:
+                navController.navigate(R.id.action_comparisonFragment_to_travelPlannerFragment);
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/example/travelplanner_0_2_1/TravelPlannerFragment.java
+++ app/src/main/java/com/example/travelplanner_0_2_1/TravelPlannerFragment.java
@@ -3,15 +3,23 @@ package com.example.travelplanner_0_2_1;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.travelplanner_0_2_1.R;
 
+import org.jetbrains.annotations.NotNull;
+
 //todo: create UI for Travel planner
-public class TravelPlannerFragment extends Fragment {
+public class TravelPlannerFragment extends Fragment implements View.OnClickListener {
+
+    private NavController navController;
+    private Button plannerToComparison;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,5 +31,25 @@ public class TravelPlannerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_travel_planner, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NotNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        plannerToComparison = view.findViewById(R.id.plannerToComparison);
+        plannerToComparison.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.plannerToComparison:
+                navController.navigate(R.id.action_travelPlannerFragment_to_comparisonFragment);
+                break;
+
+        }
     }
 }
